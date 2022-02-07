19df3d01fdfe33afb67ea1ebb7e9deca7319973d
arthurvenicio <stuks.midia@gmail.com>
2022-02-07 14:51:57 -0300

    Add project files.

---
+++ ApiCourse/Controllers/OrderController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace ApiCourse.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/[controller]")]
+    public class OrderController : ControllerBase
+    {
+    }
+}
